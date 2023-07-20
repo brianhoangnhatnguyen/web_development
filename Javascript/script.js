be6b9f8d000c367f957df743a82adc3f4daebffa
skibididopdopdopbrrr <asdfasdfasdfsadf098234502023@gmail.com>
2023-07-20 18:32:27 -0500

    efefefedfefef

--- Javascript/script.js
+++ Javascript/script.js
@@ -96,3 +96,93 @@ document.querySelector("#anotherJavascriptHeading").textContent="HIIIIIIIIIIIIII
 // console.log(3/2)
 // console.log(34%3)
 // console.log(3**2)
+
+// age = 12;
+// if (age < 18) {
+//     console.log("You cannot vote");
+// }
+// else if (age > 18) {
+//     console.log("You can vote");
+// }
+// else {
+//     console.log("Go apply for a voter id");
+// }
+
+// if(2 == 3){
+//     console.log("yes");
+// }
+// if(2 == 4){
+//     console.log("no");
+// }
+// if(2 == 5){
+//     console.log("not at all");
+// }
+// if(false == true){
+//     console.log("i dont know");
+// }
+// if(true){
+//     console.log("yes kyler!");
+// }
+// else if(4 != 5){
+//     console.log("no kyler!");
+// }
+// else if(4 != 4){
+//     console.log("okay taking yout point of view");
+// }
+// else if(!4){
+//     console.log("okaaaaayyy");
+// }
+// else{
+//     console.log("default value");
+// }
+
+// if(2 == 2){
+//     console.log("yes");
+// }
+// if(3 == 4){
+//     console.log("no");
+// }
+// if(3 == 3){
+//     console.log("not at all");
+// }
+// if(4 != 9){
+//     console.log("okay taking yout point of view");
+// }
+// if(4 != 0){
+//     console.log("Dooooooooooo it");
+// }
+// else if(4 == 9){
+//     console.log("okaaaaayyy");
+// }
+// else{
+//     console.log("default value");
+// }
+
+// Relational operators: 
+// ==, !=, >=, <=, >, <
+// == checks if two values are equal
+// != checks if two values are not equal
+// <= checks if first value is less than or equal to the second
+// >= checks if first value is greater than or equal to the second
+// > checks if the first value is greater than the second
+// < checks if the first value is less than the second
+
+// Assignment operators:
+// =, +=, -=, *=, /=, %=
+
+age = 3;
+// age += 4;
+// age = age + 4;
+
+// age -= 6;
+// age = age - 6;
+
+// age *= 3;
+// age = age * 3;
+
+// age /= 5;
+// age = age / 5;
+
+// age %= 0;
+// age = age % 0;
+console.log(age);
